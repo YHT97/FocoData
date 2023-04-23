cbfaa51a8d8bd599bf09aa0e2bf5a7847b018c22
YHD97 <ryumkin.ad@gmail.com>
2023-04-23 16:30:27 +0300

    FIX:docker image rebuild

--- src/main/java/flumine/sgm/focodata/controllers/RESTcontroller.java
+++ src/main/java/flumine/sgm/focodata/controllers/RESTcontroller.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/")
 public class RESTcontroller {
 
     final
@@ -28,6 +28,7 @@ public class RESTcontroller {
     ResponseEntity<?> index(){
         return new ResponseEntity<>(200, HttpStatus.OK);
     }
+    @CrossOrigin
     @PostMapping("/{id}/upload")
     ResponseEntity<?> upload(@RequestBody HashMap<String,Double> body, @PathVariable Long device_id){
         var key = body.keySet();
@@ -40,15 +41,6 @@ public class RESTcontroller {
         return new ResponseEntity<>("ok",HttpStatus.BAD_REQUEST);
     }
     @GetMapping("/{id}/get")
-    ResponseEntity<?> load(@PathVariable Long device_id){
-        var response = db.findAllByDevice(device_id);
-
-        if(!response.isEmpty()) {
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        }
-            return new ResponseEntity<>(null,HttpStatus.BAD_REQUEST);
-    }
-    @GetMapping("/{id}/get_p")
     ResponseEntity<?> get_page(@PathVariable Long device_id, @Param("fi") int first,@Param("li") int last,@Param("sort") String sort){
         Pageable pageable = PageRequest.of(first,last, Sort.by(sort));
         var response = db.findAllByDevice(device_id, pageable);
